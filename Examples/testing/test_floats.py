7672d7ef26c9d6c6e3c131d028104b0527d50f73
Chris Barker <PythonCHB@gmail.com>
2017-04-10 00:28:45 -0700

    added calculator example to testing

---
+++ Examples/testing/test_floats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+some simple testing examples
+"""
+
+import unittest
+import math
+
+
+class TestAlmostEqual(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_floating_point(self):
+        self.assertEqual(3 * .15, .45)
+
+    def test_almost_equal(self):
+        self.assertAlmostEqual(3 * .15, .45, places=7)
+
+    def test_almost_equal_tiny(self):
+        # these should be very differnt, but the rounding makes them both zero!
+        self.assertAlmostEqual(4 * .15e-30, .45e-30, places=7)
+
+    def test_almost_equal_huge(self):
+        # these are very close differnt, but the rounding is only done for less than zero!
+        self.assertAlmostEqual(3 * .15 * 1e30, .45 * 1e30, places=7)
+
+    def test_isclose_tiny(self):
+        # these should be very differnt, but the rounding makes them both zero!
+        self.assertTrue(math.isclose(4 * .15e-30, .45e-30))
+
+    def test_isclose_huge(self):
+        # these are very close differnt, but the rounding is only done for less than zero!
+        self.assertTrue(math.isclose(3 * .15 * 1e30, .45 * 1e30))
+
+
+if __name__ == '__main__':
+    unittest.main()
