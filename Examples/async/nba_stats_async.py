ee9a5a12fa616242cc7e2431faef6d4f06787cd8
Chris Barker <PythonCHB@gmail.com>
2017-05-30 01:42:23 -0700

    added async example

---
+++ Examples/async/nba_stats_async.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+"""
+Gathering statistics on NBA players asynchronously with the
+
+aiohttp library
+
+Running this on my machine, on my home network, took:
+
+
+***NOTE***
+On my OS-X box, a regular user is limited to 256 open files per process.
+A socket is considered a file -- so this can crash out when it hits that limit.
+
+You can increase it with:
+
+ulimit -n 2048
+
+And see what it's set to with:
+
+ulimit -a
+***********
+
+Borrowed from:
+
+http://terriblecode.com/blog/asynchronous-http-requests-in-python/
+"""
+import asyncio
+import aiohttp
+import json
+import time
+import requests
+
+base_url = 'http://stats.nba.com/stats'
+
+HEADERS = {
+    'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/45.0.2454.101 Safari/537.36'),
+}
+
+
+# # this needs to be run first before we can start -- so no need for async
+# # but making it async so we can use the aiohttp lib.
+# async def get_players(players):
+#     """
+#     get the names of all the players we are interested in
+
+#     This request will get JSON of the players for the 2016-17 season:
+
+#     http://stats.nba.com/stats/commonallplayers?LeagueID=00&season=2016-17&isonlycurrentseason=1
+#     """
+#     endpoint = '/commonallplayers'
+#     params = {'leagueid': '00', 'season': '2016-17', 'isonlycurrentseason': '1'}
+#     url = base_url + endpoint
+#     print('Getting all players...')
+#     async with aiohttp.ClientSession() as session:
+#         print("got the session")
+#         async with session.get(url, headers=HEADERS, params=params) as resp:
+#             print("got the response")
+#             data = await resp.json()
+#     players.append([(item[0], item[2]) for item in data['resultSets'][0]['rowSet']])
+
+def get_players(player_args):
+    """
+    get the names of all the players we are interested in
+
+    This request will get JSON of the players for the 2016-17 season:
+
+    http://stats.nba.com/stats/commonallplayers?LeagueID=00&season=2016-17&isonlycurrentseason=1
+
+    """
+    endpoint = '/commonallplayers'
+    params = {'leagueid': '00', 'season': '2016-17', 'isonlycurrentseason': '1'}
+    url = base_url + endpoint
+    print('Getting all players...')
+    print("about to make request")
+    resp = requests.get(url, headers=HEADERS, params=params)
+    print("got the response")
+    data = resp.json()
+    player_args.extend(
+        [(item[0], item[2]) for item in data['resultSets'][0]['rowSet']])
+
+
+# this is what we want to make concurrent
+async def get_player(player_id, player_name):
+    endpoint = '/commonplayerinfo'
+    params = {'playerid': player_id}
+    url = base_url + endpoint
+    print("Getting player", player_name)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=HEADERS, params=params) as resp:
+            print(resp)
+            all_players[name] = await resp.json()
+
+async def get_all_stats(players):
+    for id, name in players:
+        print("getting:", name)
+        all_players[name] = await get_player(id, name)
+
+all_players = {}
+players = []
+
+start = time.time()
+loop = asyncio.get_event_loop()
+
+print("getting the players")
+# loop.run_until_complete(get_players(players))
+
+get_players(players)
+print("got the players")
+
+print("getting the stats")
+loop.run_until_complete(get_all_stats(players))
+print("got the stats")
+
+# loop.run_until_complete(asyncio.gather(
+#                      *(get_player(*args) for args in players)
+#                      )
+#                     )
+
+# for id, name in players:
+#     all_players[name] = get_player(id, name)
+
+print("Done getting data: it took {:.2F} seconds".format(time.time() - start))
+
+# write it out to a file
+with open("NBA_stats_2.json", 'w') as outfile:
+    json.dump(all_players, outfile, indent=2)
+
+print("File written out")
